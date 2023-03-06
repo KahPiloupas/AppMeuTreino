d9b223353e2246b1636c9aed5bb3cdae7efd407b
Karina Piloupas <karinapiloupas@gmail.com>
2023-03-06 20:47:03 -0300

    first commit

---
+++ AppMeuTreino/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppMeuTreino
+//
+//  Created by Karina Piloupas Da Costa on 06/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
